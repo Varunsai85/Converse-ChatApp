0f6f2d9f0cec279d2f737bd39533dbf532ae3df5
Varun Sai Nadiminti <varunsainadiminti@gmail.com>
2024-12-11 18:24:09 +0530

    Login and sigup UI fixes

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -27,11 +27,11 @@ const SignUpPage = () => {
   };
 
   return (
-    <section className="flex gap-20 h-full">
-      <div className="w-1/2 text-3xl flex justify-center items-center bg-black/20 h-full px-7 shadow-box">
+    <section className="flex gap-20 h-full justify-center">
+      <div className="hidden w-1/2 text-3xl lg:flex justify-center items-center bg-base-300 h-full px-7 shadow-base-300 shadow-lg">
         Converse is a seamless chat application designed to connect you with friends, family, and colleagues. Whether you're sending quick messages or having in-depth conversations, Converse provides a reliable and user-friendly platform to stay connected. Start chatting instantly and experience the joy of effortless communication
       </div>
-      <div className="w-1/2 flex justify-center mt-10">
+      <div className="lg:w-1/2 w-[80%] flex justify-center mt-10">
         <div className="w-[80%] flex flex-col gap-7">
           <div>
             <Link to={"/"} className="flex items-center flex-col group">
